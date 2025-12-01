0e9d296546b708a424e3426d9c1eae9f4bd98ad2
Sebastian Ortega <sebastian.ortega.auriol@gmail.com>
2025-12-01 12:09:13 -0300

    Cambio de logo

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,5 +1,5 @@
 import { Link } from "react-router-dom";
-import logo from "../assets/img/logo.png";
+import logo from "../assets/img/unnamed.jpg";
 
 export default function NavBar({ total }) {
   return (
